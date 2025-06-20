dd951595b850508d765441a082eabc6efe7cc24d
Aswin-Raksha <aswinraksha2004@gmail.com>
2025-06-21 00:28:01 +0530

    initial commit

--- src/preprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def clean_description(desc):
-    return desc.lower().strip()
-
-def load_data(path):
-    df = pd.read_csv(path)
-    df['Cleaned_Description'] = df['Description'].apply(clean_description)
-    return df
